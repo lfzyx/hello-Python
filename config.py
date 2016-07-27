247bc09988b501e4457b784d73fd6f0582e48185
lfzyx <lfzyx.me@gmail.com>
2016-07-27 11:48:04 +0800

    读取配置的类

---
+++ config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Author: lfzyx
+# Contact: lfzyx.me@gmail.com
+"""
+the class use to get config sections, options, items
+
+"""
+import configparser
+
+
+# def config(file):
+#     configfile = Config(file)
+#     from_addr = configfile.__get_items__("smtp", "from_addr")
+#     return from_addr
+
+class config:
+    def __init__(self, file):
+        self.configfile = file
+        self.cc = configparser.ConfigParser()
+        self.cc.optionxform = str
+        self.cc.read(self.configfile)
+
+    def __str__(self):
+        return self.configfile
+
+    def __get_sections__(self):
+        return self.cc.sections()  # 读取每个配置项
+
+    def __get_options__(self, sections):
+        return self.cc.options(sections)  # 读取配置项中的属性列表
+
+    def __get_items__(self, sections, options):
+        return self.cc.get(sections, options)  # 读取属性对应的值
+
+
