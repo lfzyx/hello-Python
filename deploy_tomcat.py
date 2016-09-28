391d969554cc1ef63a6d629a6e22630e95db96cd
lfzyx <lfzyx.me@gmail.com>
2016-09-28 16:18:18 +0800

    删掉无用轮子后有一些bug, fix.

--- deploy_tomcat.py
+++ deploy_tomcat.py
@@ -5,10 +5,9 @@ import sys
 import zipfile
 import glob
 import os.path
-import shutil
 import time
 import subprocess
-import config
+import configparser
 
 if len(sys.argv) < 4:
     print("Usage:", sys.argv[0], "[config_path]", "[project]", "[tomcat]..")
@@ -19,9 +18,11 @@ project = sys.argv[2]
 tomcat = sys.argv[3:]
 count_of_project = len(tomcat)
 
-configfile = config.Config(config_path)
-rootpath = configfile.__get_items__("rootpath", "path")
-docBase = configfile.__get_items__("docBase", "path")
+config = configparser.ConfigParser()
+config.read(config_path)
+
+rootpath = config.get("rootpath", "path")
+docBase = config.get("docBase", "path")
 
 # 用 jenkins 产生的 jar 包会有时间命名，为了避免冲突，删除旧的 jar 包
 for jarfile in glob.glob(os.path.join(docBase, project, 'WEB-INF/lib/') + '*.jar'):
@@ -30,25 +31,25 @@ for jarfile in glob.glob(os.path.join(docBase, project, 'WEB-INF/lib/') + '*.jar
 warfile = zipfile.ZipFile(os.path.join(docBase, project+".war", ))
 warfile.extractall(os.path.join(docBase, project))
 
-for sections in configfile.__get_sections__():
+for sections in config.sections():
     '''
     根据不通的配置项打开工程中不通的配置文件，然后修改能匹配的配置项属性值
     '''
     temp = ""
     try:
-        with open(os.path.join(docBase, project, configfile.__get_items__(sections, "path")), "r", encoding="utf-8") \
+        with open(os.path.join(docBase, project, config.get(sections, "path")), "r", encoding="utf-8") \
                 as file:
             for line in file:
                 _list = line.split('=')
-                for item in configfile.__get_options__(sections):
-                    if (_list[0] == item):
-                        _list[1] = configfile.__get_items__(sections, item)
+                for item in config.options(sections):
+                    if _list[0] == item:
+                        _list[1] = config.get(sections, item)
                         line = _list[0] + '=' + _list[1] + '\n'
                 temp += line
     except (IsADirectoryError, FileNotFoundError):
         print(sys.exc_info()[1])
     else:
-        with open(os.path.join(docBase, project, configfile.__get_items__(sections, "path")), "w", encoding="utf-8") \
+        with open(os.path.join(docBase, project, config.get(sections, "path")), "w", encoding="utf-8") \
                 as file:
             file.write(temp)
 
@@ -58,5 +59,5 @@ for tomcat_T in tomcat:
     subprocess.check_call(os.path.join(rootpath, tomcat_T, project, "bin/shutdown.sh"))
     time.sleep(5)            
     subprocess.check_call(os.path.join(rootpath, tomcat_T, project, "bin/startup.sh"))
-    if count_of_project > 0 :
+    if count_of_project > 0:
         time.sleep(50)
